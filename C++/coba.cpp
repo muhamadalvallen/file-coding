86a288ade8b6d802760fb29bee26518e83448f19
muhamadalvallen <muhamadalvallen@gmail.com>
2026-06-26 16:26:11 +0700

    ini cukup

--- C++/coba.cpp
+++ C++/coba.cpp
@@ -5,6 +5,7 @@ int main() {
     std::cout << "Halo! C++ sudah jalan di ThinkPad T470s!" << std::endl;
     std::cout <<            "ini coba sendiri" << std::endl;
     std::cout << "ini juga coba sendiri" << std::endl;
+    std::cout << "oke cukup" << std::endl;
     std::cout << "========================================" << std::endl;
     return 0;
 }
