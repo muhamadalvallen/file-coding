9770ded3addb26af84fc2f7b5ec10c23a82ffd7f
muhamadalvallen <muhamadalvallen@gmail.com>
2026-06-26 15:45:29 +0700

    menambahkan file latihan C++

---
+++ coba.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "========================================" << std::endl;
+    std::cout << "Halo! C++ sudah jalan di ThinkPad T470s!" << std::endl;
+    std::cout << "========================================" << std::endl;
+    return 0;
+}
